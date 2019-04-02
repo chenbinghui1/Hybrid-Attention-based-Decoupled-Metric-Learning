c4390f238f872c3f58505e1e8994ea7f7f6288c6
chenbinghui1 <chenbinghui@bupt.edu.cn>
2019-04-02 16:25:15 +0800

    update readme.md

--- examples/CUB/DeML3-3_512/finetune_DeML3-3_512.sh
+++ examples/CUB/DeML3-3_512/finetune_DeML3-3_512.sh
@@ -2,4 +2,4 @@
 
 GLOG_logtostderr=1 /home/pris2/DeML/build/tools/caffe train \
     --solver=solver_DeML3-3_512.prototxt \
-   --weights=../pre-trained-model/3nets.caffemodel --gpu=all
+   --weights=../pre-trained-model/3nets.caffemodel --gpu=0,1
